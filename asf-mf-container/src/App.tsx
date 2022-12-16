487cc8988cc8246f981c6006e7d7303bbae22288
Laura Murria <laura.murria@nesta.org.uk>
2022-12-16 13:17:46 +0000

    Importing with the basics of the header etc from a repo based on create-react-app but has introduced an error with webpack loading of react, homepage does not render

--- asf-mf-container/src/App.tsx
+++ asf-mf-container/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./Header";
 
 import "./index.scss";
 
-const App = () => (
+export const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
     <div>Placeholder for a home page</div>
   </div>
 );
